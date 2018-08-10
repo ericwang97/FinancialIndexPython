030597561234af2fe78f02026410fcaebb85e338
Liwei Wang <ericwang97@outlook.com>
2018-08-11 01:41:47 +0800

    Delete run.py

--- news_spider/run.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from scrapy import cmdline
-
-cmdline.execute("scrapy crawl leiphone_news".split())
-# cmdline.execute("scrapy crawl _36kr_news".split())
-# cmdline.execute("scrapy crawl sina_news".split())
